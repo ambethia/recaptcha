894a9cdb6955421b3405d636e356fd51413fcc20
pivotal <pivotal@dolores.flood.pivotallabs.com>
2008-06-18 13:35:39 -0700

    Adding coverage for successful case

--- test/verify_recaptcha_test.rb
+++ test/verify_recaptcha_test.rb
@@ -9,6 +9,8 @@ class VerifyReCaptchaTest < Test::Unit::TestCase
     include Ambethia::ReCaptcha
     include Ambethia::ReCaptcha::Helper
     include Ambethia::ReCaptcha::Controller
+    
+    attr_accessor :request, :params, :session
   end
 
   def setup
@@ -18,18 +20,46 @@ class VerifyReCaptchaTest < Test::Unit::TestCase
   end
   
   def test_invalid_private_key
-    request = stub(:remote_ip => "1.1.1.1")
-    params = {:recaptcha_challenge_field => "", :recaptcha_response_field => ""}
-    session = {}
-    
-    uri = URI.parse("http://#{Ambethia::ReCaptcha::RECAPTCHA_VERIFY_SERVER}/verify")
-    post_data = {:privatekey => ENV['RECAPTCHA_PRIVATE_KEY'], :remoteip => request.remote_ip, :challenge => "", :response => ""}
-    response = stub(:body => "false\ninvalid-site-private-key")
-    Net::HTTP.expects(:post_form).with(uri, post_data).returns(response)
+    controller = TestController.new
+    controller.request = stub(:remote_ip => "1.1.1.1")
+    controller.params = {:recaptcha_challenge_field => "challenge", :recaptcha_response_field => "response"}
+    controller.session = {}
 
-    controller = TestController.new(request, params, session)
+    post_data = {}
+    post_data[:privatekey] = ENV['RECAPTCHA_PRIVATE_KEY']
+    post_data[:remoteip] = controller.request.remote_ip
+    post_data[:challenge] = "challenge"
+    post_data[:response] = "response"
+    
+    response = stub(:body => "false\ninvalid-site-private-key")
+    Net::HTTP.expects(:post_form).with(expected_uri, post_data).returns(response)
+
     assert !controller.verify_recaptcha
     
-    assert_equal "invalid-site-private-key", session[:recaptcha_error]
+    assert_equal "invalid-site-private-key", controller.session[:recaptcha_error]
   end
+  
+  def test_success
+    controller = TestController.new
+    controller.request = stub(:remote_ip => "1.1.1.1")
+    controller.params = {:recaptcha_challenge_field => "challenge", :recaptcha_response_field => "response"}
+    controller.session = {}
+
+    post_data = {}
+    post_data[:privatekey] = ENV['RECAPTCHA_PRIVATE_KEY']
+    post_data[:remoteip] = controller.request.remote_ip
+    post_data[:challenge] = "challenge"
+    post_data[:response] = "response"
+    
+    response = stub(:body => "true\n")
+    Net::HTTP.expects(:post_form).with(expected_uri, post_data).returns(response)
+
+    assert controller.verify_recaptcha
+    
+    assert_nil controller.session[:recaptcha_error]
+  end
+  
+  def expected_uri
+    URI.parse("http://#{Ambethia::ReCaptcha::RECAPTCHA_VERIFY_SERVER}/verify")
+  end    
 end
