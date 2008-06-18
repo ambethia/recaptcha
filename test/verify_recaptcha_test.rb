2b2eabfc63928a8d2752a7ae5874664b28f30361
pivotal <pivotal@dolores.flood.pivotallabs.com>
2008-06-18 13:39:08 -0700

    Adding coverage for successful case

--- test/verify_recaptcha_test.rb
+++ test/verify_recaptcha_test.rb
@@ -5,18 +5,6 @@ require 'net/http'
 require File.dirname(__FILE__) + '/../lib/recaptcha'
 
 class VerifyReCaptchaTest < Test::Unit::TestCase
-  class TestController < Struct.new(:request, :params, :session)
-    include Ambethia::ReCaptcha
-    include Ambethia::ReCaptcha::Helper
-    include Ambethia::ReCaptcha::Controller
-    
-    attr_accessor :request, :params, :session
-    
-    def initialize
-      @session = {}
-    end
-  end
-
   def setup
     ENV['RECAPTCHA_PUBLIC_KEY']  = '0000000000000000000000000000000000000000'
     ENV['RECAPTCHA_PRIVATE_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
@@ -47,6 +35,20 @@ class VerifyReCaptchaTest < Test::Unit::TestCase
 
     assert @controller.verify_recaptcha
     assert_nil @controller.session[:recaptcha_error]
+  end
+  
+  private
+  
+  class TestController < Struct.new(:request, :params, :session)
+    include Ambethia::ReCaptcha
+    include Ambethia::ReCaptcha::Helper
+    include Ambethia::ReCaptcha::Controller
+    
+    attr_accessor :request, :params, :session
+    
+    def initialize
+      @session = {}
+    end
   end
   
   def response_with_body(body)
