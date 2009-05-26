c205b8c2eb9cf6cca55843d50005124f6aafb805
introkun <introkun@gmail.com>
2009-05-26 13:14:42 -0700

    Added parameter :message => 'Custom error message' for verify_recaptcha method.

--- lib/recaptcha.rb
+++ lib/recaptcha.rb
@@ -67,9 +67,9 @@ module Ambethia
             if model
               model.valid?
               if Rails::VERSION::MAJOR == 2 and Rails::VERSION::MINOR >= 2
-                model.errors.add_to_base I18n.translate("#{model.class.name.underscore}.captcha", :scope => %w(activerecord errors models), :default => "Captcha response is incorrect, please try again.")
+                model.errors.add_to_base I18n.translate("#{model.class.name.underscore}.captcha", :scope => %w(activerecord errors models), :default => options[:message] ||= "Captcha response is incorrect, please try again.")
               else
-                model.errors.add_to_base "Captcha response is incorrect, please try again."
+                model.errors.add_to_base options[:message] ||= "Captcha response is incorrect, please try again."
               end
             end
             return false
