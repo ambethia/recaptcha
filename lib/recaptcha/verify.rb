ad8bbbb0422d356b1a3a4bce553a67fee161c22f
chrisfinne <chris.finne@gmail.com>
2009-09-14 03:04:57 -0700

    Add a timout to the validator
    Signed-off-by: Jason L Perry <jasper@ambethia.com>

--- lib/recaptcha/verify.rb
+++ lib/recaptcha/verify.rb
@@ -9,28 +9,34 @@ module Recaptcha
       private_key ||= ENV['RECAPTCHA_PRIVATE_KEY']
       raise RecaptchaError, "No private key specified." unless private_key
       begin
-        recaptcha = Net::HTTP.post_form URI.parse("http://#{RECAPTCHA_VERIFY_SERVER}/verify"), {
-          "privatekey" => private_key,
-          "remoteip"   => request.remote_ip,
-          "challenge"  => params[:recaptcha_challenge_field],
-          "response"   => params[:recaptcha_response_field]
-        }
+        recaptcha = nil
+        Timeout::timeout(options[:timeout] || 3) do
+          recaptcha = Net::HTTP.post_form URI.parse("http://#{RECAPTCHA_VERIFY_SERVER}/verify"), {
+            "privatekey" => private_key,
+            "remoteip"   => request.remote_ip,
+            "challenge"  => params[:recaptcha_challenge_field],
+            "response"   => params[:recaptcha_response_field]
+          }
+        end
         answer, error = recaptcha.body.split.map { |s| s.chomp }
         unless answer == 'true'
           session[:recaptcha_error] = error
           if model
             model.valid?
-            if Rails::VERSION::MAJOR == 2 and Rails::VERSION::MINOR >= 2
-              model.errors.add :base, I18n.translate("#{model.class.name.underscore}.captcha", :scope => %w(errors models), :default => (options[:message] || "Captcha response is incorrect, please try again."))
-            else
-              model.errors.add :base, options[:message] || "Captcha response is incorrect, please try again."
-            end
+            model.errors.add :base, options[:message] || "Captcha response is incorrect, please try again."
           end
           return false
         else
           session[:recaptcha_error] = nil
           return true
         end
+      rescue Timeout::Error 
+        session[:recaptcha_error] = "recaptcha-not-reachable"
+        if model
+          model.valid?
+          model.errors.add :base, options[:message] || "Oops, we failed to validate your Captcha. Please try again."
+        end
+        return false
       rescue Exception => e
         raise RecaptchaError, e.message, e.backtrace
       end
