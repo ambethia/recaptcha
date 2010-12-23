6b9325348528681b2aa33a86ecacc6335426de7f
PavelT <paveltyk@gmail.com>
2010-12-23 20:50:37 +0200

    Added better AR integration.

--- lib/recaptcha/rails.rb
+++ lib/recaptcha/rails.rb
@@ -2,3 +2,5 @@ require 'recaptcha'
 
 ActionView::Base.send(:include, Recaptcha::ClientHelper)
 ActionController::Base.send(:include, Recaptcha::Verify)
+ActiveRecord::Base.send(:include, Recaptcha::ActiveRecordVerify::InstanceMethods)
+
