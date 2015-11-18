1dc61218ef2b7d3b0ffe9172f8dde943a05d4bdc
Michael Grosser <michael@grosser.it>
2015-11-18 10:29:58 -0800

    fix

--- lib/recaptcha/verify.rb
+++ lib/recaptcha/verify.rb
@@ -10,7 +10,7 @@ module Recaptcha
     def verify_recaptcha(options = {})
       options = {:model => options} unless options.is_a? Hash
 
-      env_options = options[:env] || ENV['RAILS_ENV'] || Rails.env if defined? Rails.env
+      env_options = options[:env] || ENV['RAILS_ENV'] || (Rails.env if defined? Rails.env)
       return true if Recaptcha.configuration.skip_verify_env.include? env_options
       model = options[:model]
       attribute = options[:attribute] || :base
