aee249b437a813d1963e4235e64018029e623038
Michael Grosser <michael@grosser.it>
2015-11-20 20:05:50 -0800

    cleanup

--- lib/recaptcha/client_helper.rb
+++ lib/recaptcha/client_helper.rb
@@ -3,69 +3,48 @@ module Recaptcha
     # Your public API can be specified in the +options+ hash or preferably
     # using the Configuration.
     def recaptcha_tags(options = {})
-      public_key   = options[:public_key] ||= Recaptcha.configuration.public_key
+      public_key = options[:public_key] || Recaptcha.configuration.public_key
       raise RecaptchaError, "No public key specified." unless public_key
-      private_key  = options[:private_key] ||= Recaptcha.configuration.private_key
-      raise RecaptchaError, "No private key specified." unless private_key
-      error = options[:error] ||= ((defined? flash) ? flash[:recaptcha_error] : "") # TODO not being used !?
-      uri   = Recaptcha.configuration.api_server_url(ssl: options[:ssl])
-      uri += "?hl=#{options[:hl]}" unless options[:hl].blank?
 
-      html = ""
-      html << %{<script src="#{uri}" async defer></script>\n}
+      script_url = Recaptcha.configuration.api_server_url(ssl: options[:ssl])
+      script_url += "?hl=#{options[:hl]}" unless options[:hl].blank?
+      fallback_uri = "#{script_url.chomp('.js')}/fallback?k=#{public_key}"
 
       data_attributes = options.slice(:theme, :type, :callback, :expired_callback, :size)
       data_attributes[:sitekey] = public_key
+      data_attributes[:stoken] = Recaptcha::Token.secure_token if options[:stoken] != false
+      data_attributes = data_attributes.map { |k,v| %{data-#{k.to_s.tr('_','-')}="#{v}"} }.join(" ")
 
-      if options[:stoken] != false
-        data_attributes[:stoken] = Recaptcha::Token.secure_token
-      end
-
-      data_attributes = data_attributes.map {|k,v| %{data-#{k.to_s.gsub(/_/,'-')}="#{v}"} }.join(" ")
-
+      html = %{<script src="#{script_url}" async defer></script>\n}
       html << %{<div class="g-recaptcha" #{data_attributes}></div>\n}
 
-      unless options[:noscript] == false
-        fallback_uri = "#{uri.chomp('.js')}/fallback?k=#{public_key}"
-        html << %{<noscript>}
-        html << %{<div style="width: 302px; height: 352px;">}
-        html << %{  <div style="width: 302px; height: 352px; position: relative;">}
-        html << %{    <div style="width: 302px; height: 352px; position: absolute;">}
-        html << %{      <iframe src="#{fallback_uri}"}
-        html << %{                frameborder="0" scrolling="no"}
-        html << %{                style="width: 302px; height:352px; border-style: none;">}
-        html << %{        </iframe>}
-        html << %{      </div>}
-        html << %{      <div style="width: 250px; height: 80px; position: absolute; border-style: none; }
-        html << %{             bottom: 21px; left: 25px; margin: 0px; padding: 0px; right: 25px;">}
-        html << %{        <textarea id="g-recaptcha-response" name="g-recaptcha-response" }
-        html << %{                  class="g-recaptcha-response" }
-        html << %{                  style="width: 250px; height: 80px; border: 1px solid #c1c1c1; }
-        html << %{                  margin: 0px; padding: 0px; resize: none;" value=""> }
-        html << %{        </textarea>}
-        html << %{      </div>}
-        html << %{    </div>}
-        html << %{  </div>}
-        html << %{</noscript>}
+      if options[:noscript] != false
+        html << <<-HTML
+          <noscript>
+            <div style="width: 302px; height: 352px;">
+              <div style="width: 302px; height: 352px; position: relative;">
+                <div style="width: 302px; height: 352px; position: absolute;">
+                  <iframe
+                    src="#{fallback_uri}"
+                    frameborder="0" scrolling="no"
+                    style="width: 302px; height:352px; border-style: none;">
+                  </iframe>
+                </div>
+                <div style="width: 250px; height: 80px; position: absolute; border-style: none;
+                  bottom: 21px; left: 25px; margin: 0px; padding: 0px; right: 25px;">
+                  <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+                    class="g-recaptcha-response"
+                    style="width: 250px; height: 80px; border: 1px solid #c1c1c1;
+                    margin: 0px; padding: 0px; resize: none;" value="">
+                  </textarea>
+                </div>
+              </div>
+            </div>
+          </noscript>
+        HTML
       end
 
-      (html.respond_to?(:html_safe) && html.html_safe) || html
-    end
-
-    private
-
-    def hash_to_json(hash)
-      result = "{"
-      result << hash.map do |k, v|
-        if v.is_a?(Hash)
-          "\"#{k}\": #{hash_to_json(v)}"
-        elsif ! v.is_a?(String) || k.to_s =~ %r{(callback|expired-callback)}
-          "\"#{k}\": #{v}"
-        else
-          "\"#{k}\": \"#{v}\""
-        end
-      end.join(", ")
-      result << "}"
+      html.respond_to?(:html_safe) ? html.html_safe : html
     end
   end
 end
