8d348e31dbcfef75491e8ee50b83bc5b8c35c938
Michael Grosser <michael@grosser.it>
2015-11-30 15:54:56 -0800

    do not define rails

--- lib/recaptcha/rails.rb
+++ lib/recaptcha/rails.rb
@@ -1,13 +1,11 @@
 require 'net/http'
 require 'recaptcha'
 
-module Rails
-  module Recaptcha
-    class Railtie < Rails::Railtie
-      initializer :recaptcha do
-        ActionView::Base.send(:include, ::Recaptcha::ClientHelper)
-        ActionController::Base.send(:include, ::Recaptcha::Verify)
-      end
+module Recaptcha
+  class Railtie < Rails::Railtie
+    initializer :recaptcha do
+      ActionView::Base.send(:include, ::Recaptcha::ClientHelper)
+      ActionController::Base.send(:include, ::Recaptcha::Verify)
     end
   end
 end
