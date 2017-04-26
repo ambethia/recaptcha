d2741a95933158c37f9a438f2513c8a1a0d5645d
Sheng Loong Su <sushengloong@gmail.com>
2017-04-26 21:47:12 +0800

    Make api_server_url and verify_url configurable
    This is so that the URL can be set to some proxies instead of the
    hardcoded Google links.
    This commit is inspired by https://github.com/ambethia/recaptcha/issues/214\#issuecomment-285257412

--- lib/recaptcha/configuration.rb
+++ lib/recaptcha/configuration.rb
@@ -28,7 +28,8 @@ module Recaptcha
   #   end
   #
   class Configuration
-    attr_accessor :skip_verify_env, :secret_key, :site_key, :proxy, :handle_timeouts_gracefully, :hostname
+    attr_accessor :skip_verify_env, :secret_key, :site_key, :api_server_url, :verify_url, :proxy,
+      :handle_timeouts_gracefully, :hostname
 
     def initialize #:nodoc:
       @skip_verify_env            = %w[test cucumber]
@@ -36,6 +37,9 @@ module Recaptcha
 
       @secret_key           = ENV['RECAPTCHA_SECRET_KEY']
       @site_key             = ENV['RECAPTCHA_SITE_KEY']
+
+      @api_server_url       = ENV['RECAPTCHA_API_SERVER_URL']
+      @verify_url           = ENV['RECAPTCHA_VERIFY_URL']
     end
 
     def secret_key!
@@ -47,11 +51,11 @@ module Recaptcha
     end
 
     def api_server_url
-      CONFIG.fetch('server_url')
+      @api_server_url || CONFIG.fetch('server_url')
     end
 
     def verify_url
-      CONFIG.fetch('verify_url')
+      @verify_url || CONFIG.fetch('verify_url')
     end
   end
 end
