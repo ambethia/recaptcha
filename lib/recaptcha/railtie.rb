25051a683ee0cc095e8312f442905e011bfc9e1e
Yauheni Dakuka <yauheni.dakuka@gmail.com>
2018-06-24 23:17:29 +0300

    Remove initializer :recaptcha

--- lib/recaptcha/railtie.rb
+++ lib/recaptcha/railtie.rb
@@ -2,16 +2,14 @@
 
 module Recaptcha
   class Railtie < Rails::Railtie
-    initializer :recaptcha do
-      ActiveSupport.on_load(:action_view) do
-        require 'recaptcha/client_helper'
-        include Recaptcha::ClientHelper
-      end
+    ActiveSupport.on_load(:action_view) do
+      require 'recaptcha/client_helper'
+      include Recaptcha::ClientHelper
+    end
 
-      ActiveSupport.on_load(:action_controller) do
-        require 'recaptcha/verify'
-        include Recaptcha::Verify
-      end
+    ActiveSupport.on_load(:action_controller) do
+      require 'recaptcha/verify'
+      include Recaptcha::Verify
     end
   end
 end
