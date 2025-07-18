05847ea45a5608884c360d5896a9fa33f59866a0
Ruben Ignacio Guzman Zamora <ruben.ignaciogz@gmail.com>
2025-07-17 20:05:31 -0500

    Add Reply class to handle Recaptcha responses

--- lib/recaptcha.rb
+++ lib/recaptcha.rb
@@ -6,6 +6,7 @@ require 'uri'
 
 require 'recaptcha/configuration'
 require 'recaptcha/helpers'
+require 'recaptcha/reply'
 require 'recaptcha/adapters/controller_methods'
 require 'recaptcha/adapters/view_methods'
 if defined?(Rails)
@@ -76,15 +77,14 @@ module Recaptcha
     body['event']['expectedAction'] = options[:action] if options.key?(:action)
     body['event']['userIpAddress'] = options[:remote_ip] if options.key?(:remote_ip)
 
-    reply = api_verification_enterprise(query_params, body, project_id, timeout: options[:timeout])
-    score = reply.dig('riskAnalysis', 'score')
-    token_properties = reply['tokenProperties']
-    success = !token_properties.nil? &&
-      token_properties['valid'].to_s == 'true' &&
-      hostname_valid?(token_properties['hostname'], options[:hostname]) &&
-      action_valid?(token_properties['action'], options[:action]) &&
-      score_above_threshold?(score, options[:minimum_score]) &&
-      score_below_threshold?(score, options[:maximum_score])
+    raw_reply = api_verification_enterprise(query_params, body, project_id, timeout: options[:timeout])
+    reply = Reply.new(raw_reply, enterprise: true)
+
+    success = reply.success? &&
+      hostname_valid?(reply.hostname, options[:hostname]) &&
+      action_valid?(reply.action, options[:action]) &&
+      score_above_threshold?(reply.score, options[:minimum_score]) &&
+      score_below_threshold?(reply.score, options[:maximum_score])
 
     if options[:with_reply] == true
       [success, reply]
@@ -98,12 +98,14 @@ module Recaptcha
     verify_hash = { 'secret' => secret_key, 'response' => response }
     verify_hash['remoteip'] = options[:remote_ip] if options.key?(:remote_ip)
 
-    reply = api_verification_free(verify_hash, timeout: options[:timeout], json: options[:json])
-    success = reply['success'].to_s == 'true' &&
-      hostname_valid?(reply['hostname'], options[:hostname]) &&
-      action_valid?(reply['action'], options[:action]) &&
-      score_above_threshold?(reply['score'], options[:minimum_score]) &&
-      score_below_threshold?(reply['score'], options[:maximum_score])
+    raw_reply = api_verification_free(verify_hash, timeout: options[:timeout], json: options[:json])
+    reply = Reply.new(raw_reply, enterprise: false)
+
+    success = reply.success? &&
+      hostname_valid?(reply.hostname, options[:hostname]) &&
+      action_valid?(reply.action, options[:action]) &&
+      score_above_threshold?(reply.score, options[:minimum_score]) &&
+      score_below_threshold?(reply.score, options[:maximum_score])
 
     if options[:with_reply] == true
       [success, reply]
---
+++ lib/recaptcha/reply.rb
@@ -0,0 +1,77 @@
+module Recaptcha
+  class Reply
+    attr_reader :raw_reply, :enterprise
+
+    def initialize(raw_reply, enterprise: false)
+      @raw_reply = raw_reply
+      @enterprise = enterprise
+    end
+
+    def [](key)
+      return send(key.to_sym) if respond_to?(key.to_sym)
+
+      raw_reply[key]
+    end
+
+    def token_properties
+      raw_reply['tokenProperties'] if enterprise?
+    end
+
+    def success?
+      success = enterprise? ? token_properties&.dig('valid') : raw_reply['success']
+
+      success.to_s == 'true'
+    end
+
+    def hostname
+      return raw_reply['hostname'] unless enterprise?
+
+      token_properties&.dig('hostname')
+    end
+
+    def action
+      return raw_reply['action'] unless enterprise?
+
+      token_properties&.dig('action')
+    end
+
+    def score
+      return raw_reply['score'] unless enterprise?
+
+      raw_reply.dig('riskAnalysis', 'score')
+    end
+
+    def error_codes
+      return [] if enterprise?
+
+      raw_reply['error-codes'] || []
+    end
+
+    # Returns the challenge timestamp
+    def challenge_ts
+      return raw_reply['challenge_ts'] unless enterprise?
+
+      token_properties&.dig('createTime')
+    end
+
+    def enterprise?
+      @enterprise
+    end
+
+    def free?
+      !enterprise?
+    end
+
+    def to_h
+      raw_reply
+    end
+
+    def to_s
+      raw_reply.to_s
+    end
+
+    def to_json
+      raw_reply.to_json
+    end
+  end
+end
